fddfcaa686558ae43bc32ce9559b86fe1d6977ca
Bertrand Lamy <bertrand.lamy@gmail.com>
2020-04-23 10:10:13 +0200

    Fix Heroku deployement
    * removed config files, they need to be generated to get proper PORT variable
    * generate config files in setup.sh
    * upgrade Pyhton to 3.8.2

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 mkdir -p ~/.streamlit/
 
-cp streamlit-config/*.toml ~/.streamlit/
+echo "\
+[general]\n\
+email = \"\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
